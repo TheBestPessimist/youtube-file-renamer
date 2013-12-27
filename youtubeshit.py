17d36687127f2728846957683de855dc77b4e36e
Cristian Viorel Pasat <cristian.nahsuc@gmail.com>
2013-12-27 21:30:52 +0200

    Ported to python 3.

--- youtubeshit.py
+++ youtubeshit.py
@@ -5,17 +5,14 @@ Created on 30.09.2012
 '''
 
 #-------------------------------------------------------------------------------
-# script used for renaming files downloaded with jdownloader 2 from youtube.
-# it takes the files and moves them to a specified folder.
-# use linux style folder delimiters ('/' and not '\\')
+# Script used for renaming files downloaded with jdownloader 2 from youtube.
+# It takes the files and moves them to a specified folder.
+# Use linux style folder delimiters ('/' and not '\\')
 #-------------------------------------------------------------------------
 
 import os
 import shutil
 import sys
-# to force using utf8
-reload(sys)
-sys.setdefaultencoding("utf-8")
 
 
 def findNewFileName(oldFileName):
@@ -38,9 +35,8 @@ def findNewFileName(oldFileName):
 
 
 def renameThem(path, newPath):
-    
     if not os.path.exists(newPath):
-        print 'New path does not exist, creating...'
+        print ('New path does not exist, creating...')
         os.makedirs(newPath)
 
     songCounter = 0
@@ -55,10 +51,10 @@ def renameThem(path, newPath):
         newFileName = findNewFileName(oldFileName)
         newFilePath = os.path.join(newPath, newFileName)
 
-        print songCounter, oldFilePath
-        print newFileName
-        print newFilePath
-        print
+        print(songCounter, oldFilePath)
+        print(newFileName)
+        print(newFilePath)
+        print()
 
         # this might throw a duplicate error. needs further testing
         shutil.copy2(oldFilePath, newFilePath)
@@ -67,11 +63,16 @@ def renameThem(path, newPath):
     # print 'Songs processed: ', songCounter
 
 
-if __name__ == '__main__':
-    # redirect stdout
+def main():
+    # to redirect stdout
     # sys.stdout = open(os.path.join(os.environ['USERPROFILE'],
     # 'Desktop/rename log.txt'), 'w')
-    path = os.path.normpath(u"E:/zzmusic/aac 112/xxx/")
-    newPath = os.path.normpath(u"C:/youtube/")
+    path = os.path.normpath('C:/Users/CristianViorel/Desktop/PLM/xxx/1')
+    newPath = os.path.normpath('C:/Users/CristianViorel/Desktop/PLM/xxx/2')
 
     renameThem(path, newPath)
+
+
+
+if __name__ == '__main__':
+    main()
