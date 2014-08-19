38fb61ec915e614015509a63136d6db0f94630b9
Cristian Viorel Pasat <cristian.nahsuc@gmail.com>
2014-08-19 20:41:23 +0300

    Rewrite find new file name.

--- youtubeshit.py
+++ youtubeshit.py
@@ -48,27 +48,48 @@ def renameThem(path, newPath):
     for oldFilePath in filesFullPath:
         songCounter += 1
         oldFileName = os.path.basename(oldFilePath)
-        newFileName = findNewFileName(oldFileName)
+        newFileName = find_new_file_name_v2(oldFileName)
         newFilePath = os.path.join(newPath, newFileName)
 
-        print(songCounter, oldFilePath)
-        print(newFileName)
-        print(newFilePath)
-        print()
+        # this throws so many errors... fml and unicode... :-(
+        # print(songCounter, oldFilePath)
+        # print(newFileName)
+        # print(newFilePath)
+        # print()
 
         # this might throw a duplicate error. needs further testing
         shutil.copy2(oldFilePath, newFilePath)
 
-    # for debug info
-    # print 'Songs processed: ', songCounter
+
+def find_new_file_name_v2(oldFileName):
+    oldFileName = remove_OFM(oldFileName)
+    indexToDelete = oldFileName.rfind('(')
+
+    # if found a new name to edit
+    extension = (os.path.splitext(oldFileName))[1]
+    newFileName = oldFileName[:indexToDelete] + extension
+    return newFileName
+
+
+def remove_OFM(oldFileName):
+    """Remove the "(Official music video)" shit from the name"""
+
+    newFileName = oldFileName
+    poz1 = oldFileName.lower().find('(offi')
+    poz2 = oldFileName.lower().find('video)', poz1) + len('video)')
+    if poz1 > 0 and poz2 > 0:
+        newFileName = oldFileName[:poz1] + oldFileName[poz2:]
+        print(poz1, poz2)
+    return newFileName
 
 
 def main():
     # to redirect stdout
     # sys.stdout = open(os.path.join(os.environ['USERPROFILE'],
-    # 'Desktop/rename log.txt'), 'w')
-    path = os.path.normpath('C:/Users/CristianViorel/Desktop/PLM/xxx/1')
-    newPath = os.path.normpath('C:/Users/CristianViorel/Desktop/PLM/xxx/2')
+    # 'Desktop/rename log.txt'), 'w+')
+    # path = os.path.normpath('C:/Users/CristianViorel/Desktop/PLM/xxx/1')
+    path = os.path.normpath("E:/xxx/new - Copy")
+    newPath = os.path.normpath('C:/xxx/')
 
     renameThem(path, newPath)
 
